877c981532742013406eee6b08551ecf7992f527
ANDRZEJ ROMAN <Sebastian97x@gmail.com>
2019-06-13 19:38:32 +0200

    First Commit

---
+++ src/main/java/com/kodilla/KodillaCourseeApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseeApplication.class, args);
+    }
+
+}
