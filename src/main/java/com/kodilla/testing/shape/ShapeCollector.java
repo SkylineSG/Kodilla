f4de89b8ba7aa7997d5554297b820d0e7f8b184d
SkylineSG <Sebastian97x@gmail.com>
2019-06-15 21:01:34 +0200

    4

---
+++ src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,94 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+
+interface Shape {
+   void shapeCollector(String name);
+   void getField(int field);
+}
+
+class Triangle implements Shape {
+
+    String name = "Triangle";
+    private int a = 2;
+    private int h = 5;
+    int field = (a*h) / 2;
+
+    public Triangle(String name, int a, int h) {
+        this.name = name;
+        this.a = a;
+        this.h = h;
+    }
+
+    public void shapeCollector(String name) {
+        System.out.println(name);
+    }
+    public void getField(int field){
+        System.out.println(field);
+    }
+}
+
+class Square implements Shape{
+
+    private String name = "Square";
+    private int a=3;
+    private int field= a*a;
+
+    public Square(String name, int a) {
+        this.name = name;
+        this.a = a;
+    }
+
+    public void shapeCollector(String name) {
+        System.out.println(name);
+    }
+    public void getField(int field){
+        System.out.println(field);
+    }
+}
+
+class Circle implements Shape{
+
+    private String name = "Circle";
+    private double pi = 3.14;
+    private int r=3;
+    private double field = (r*r)*pi;
+
+    public Circle(String name, double pi, int r) {
+        this.name = name;
+        this.pi = pi;
+        this.r = r;
+    }
+
+    public void shapeCollector(String name) {
+        System.out.println(name);
+    }
+    public void getField(int field){
+        System.out.println(field);
+    }
+}
+
+class ShapeCollector {
+
+    ArrayList<Circle> circles = new ArrayList<>();
+    ArrayList<Triangle> triangles = new ArrayList<>();
+    ArrayList<Square> squares = new ArrayList<>();
+
+
+    public ShapeCollector(ArrayList<Circle> circles) {
+        this.circles = circles;
+    }
+
+    public void addFigure(Shape shape) {
+
+    }
+
+    public void removeFromList(Shape shape) {
+
+    }
+
+    public void getFigure(int n) {
+
+    }
+}
+
---
+++ src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.testing.shape;
+
+import org.junit.*;
+
+public class ShapeCollectorTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testAddFigure() {
+        //Given
+        Circle circle1 = new Circle("Circle", 3.14,7);
+
+        //When
+        circle1
+
+        //Then
+
+    }
+    @Test
+    public void testRemoveFigure() {
+
+
+    }
+    @Test
+    public void testGetFigure(){
+
+    }
+}
