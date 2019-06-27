3c4ec0d641ef5108bff4d445e5887efea30fc2a6
SkylineSG <Sebastian97x@gmail.com>
2019-06-27 16:29:34 +0200

    n

--- kodilla-streeam/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
+++ kodilla-streeam/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -1,65 +1,65 @@
-package com.kodilla.stream.forumuser;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-
-public final class ForumUser {
-
-    private final int ID;
-    private final String user;
-    private final char gender;
-    private final LocalDate birthDate;
-    private final int posts;
-    LocalDate start = LocalDate.of(birthDate.getYear(),birthDate.getMonth(),birthDate.getDayOfMonth());
-
-
-    public ForumUser(int ID, String user, char gender,int yearOfBirth,
-                     int monthOfBirth, int dayOfBirth, int posts) {
-        this.ID = ID;
-        this.user = user;
-        this.gender = gender;
-        this.birthDate = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);
-        this.posts = posts;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public char getGender() {
-        return gender;
-    }
-
-    public LocalDate getDate() {
-        return birthDate;
-    }
-    public LocalDate getActualDate() {
-        return birthDate;
-    }
-
-    public int getYear() {
-        return birthDate.getYear();
-   }
-
-    public int getPosts() {
-        return posts;
-    }
-
-    @Override
-    public String toString() {
-        return "ForumUser{" +
-                "ID=" + ID +
-                ", user='" + user + '\'' +
-                ", gender=" + gender +
-                ", date=" + birthDate +
-                ", posts=" + posts +
-                '}';
-    }
-}
+//package com.kodilla.stream.forumuser;
+//
+//import java.text.DateFormat;
+//import java.text.SimpleDateFormat;
+//import java.time.LocalDate;
+//import java.util.Calendar;
+//import java.util.Date;
+//
+//public final class ForumUser {
+//
+//    private final int ID;
+//    private final String user;
+//    private final char gender;
+//    private final LocalDate birthDate;
+//    private final int posts;
+//    LocalDate start = LocalDate.of(birthDate.getYear(),birthDate.getMonth(),birthDate.getDayOfMonth());
+//
+//
+//    public ForumUser(int ID, String user, char gender,int yearOfBirth,
+//                     int monthOfBirth, int dayOfBirth, int posts) {
+//        this.ID = ID;
+//        this.user = user;
+//        this.gender = gender;
+//        this.birthDate = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);
+//        this.posts = posts;
+//    }
+//
+//    public int getID() {
+//        return ID;
+//    }
+//
+//    public String getUser() {
+//        return user;
+//    }
+//
+//    public char getGender() {
+//        return gender;
+//    }
+//
+//    public LocalDate getDate() {
+//        return birthDate;
+//    }
+//    public LocalDate getActualDate() {
+//        return birthDate;
+//    }
+//
+//    public int getYear() {
+//        return birthDate.getYear();
+//   }
+//
+//    public int getPosts() {
+//        return posts;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ForumUser{" +
+//                "ID=" + ID +
+//                ", user='" + user + '\'' +
+//                ", gender=" + gender +
+//                ", date=" + birthDate +
+//                ", posts=" + posts +
+//                '}';
+//    }
+//}
