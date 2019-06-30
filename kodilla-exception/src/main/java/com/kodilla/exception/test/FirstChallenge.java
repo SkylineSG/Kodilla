d5053b6c20608e2ea9e03c3bc2d0af47dd6e3277
SkylineSG <Sebastian97x@gmail.com>
2019-06-30 19:02:57 +0200

    3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,39 +1,31 @@
 package com.kodilla.exception.test;
 
-import java.util.stream.Stream;
+import java.io.IOException;
 
 public class FirstChallenge {
-    public double divide(double a, double b)
-        /* throws ArithmeticException */{
-   /*     if(b == 0){
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
             throw new ArithmeticException();
         }
         return a / b;
-        */
-        double resoult = 0;
-
-        try {
-            resoult = (int)a / (int)b;
-        }catch (ArithmeticException e){
-            System.out.println("Do not devide by 0");
-        }finally {
-            System.out.println("Value a: "+ a+ "  "+"Value b: "+b);
-        }
-        return resoult;
     }
 
     /**
      * This main can throw an ArithmeticException!!!
+     *
      * @param args
      */
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
-
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("b = 0 " + e);
+        } finally {
+            System.out.println("end of operation");
+        }
     }
-
 }
+
