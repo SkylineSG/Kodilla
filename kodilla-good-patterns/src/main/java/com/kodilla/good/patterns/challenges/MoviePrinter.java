a5981963051cfed030491dd19aaedc0c36aafcfe
SkylineSG <Sebastian97x@gmail.com>
2019-07-30 19:10:16 +0200

    task

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MoviePrinter.java
@@ -0,0 +1,21 @@
+package com.kodilla.good.patterns.challenges;
+
+
+import java.util.stream.Collectors;
+
+public class MoviePrinter {
+    public static void main(String[] args){
+
+        MovieStore movies = new MovieStore();
+
+        String moviesToDisplay = movies.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("  !  "));
+
+        System.out.println("Movies:");
+        System.out.println(moviesToDisplay);
+
+
+
+    }
+}
