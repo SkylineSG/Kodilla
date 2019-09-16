f1b49d11e0ee27737be8de160018b2b0c465f862
SkylineSG <Sebastian97x@gmail.com>
2019-09-16 18:07:44 +0200

    TaskList

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -8,57 +8,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "TASKLISTS")
+@Table(name="TASKLISTS")
 public class TaskList {
-
     private int id;
     private String listName;
     private String description;
-    private List<Task> tasks =new ArrayList<>();
+    private List<Task> tasks = new ArrayList<>();
 
-    public TaskList(){}
-
-    public TaskList( String listName, String description) {
-        this.listName = listName;
-        this.description = description;
-    }
-
-    @Id
-    @GeneratedValue
-    @NotNull
-    @Column(name = "ID", unique = true)
-    public int getId() {
-        return id;
-    }
-
-    @NotNull
-    @Column(name = "LISTNAME")
-    public String getListName() {
-        return listName;
-    }
-
-    @Column(name = "DESCRIPTION")
-    public String getDescription() {
-        return description;
-    }
-
-    public void setId(int id) {
+    public TaskList(int id, String listName, String description) {
         this.id = id;
-    }
-
-    public void setListName(String listName) {
         this.listName = listName;
+        this.description = description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public TaskList() {
     }
 
     public List<Task> getTasks() {
         return tasks;
     }
 
-    public void setTasks(List<Task> tasks) {
+    private void setTasks(List<Task> tasks) {
         this.tasks = tasks;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name="ID",unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name="LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name="DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -9,33 +9,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TaskList.class)
 public class TaskListDaoTestSuite {
-
-    @Autowired
-    private TaskDao taskDao;
+    private static final String LISTNAME = "The next one list";
     @Autowired
     private TaskListDao taskListDao;
-    private static final String LISTNAME = "OneList";
+    @Autowired
+    private TaskDao taskDao;
 
     @Test
-    public void testFindByListName() {
+    public void testTaskDaoFindByListName() {
         //Given
-        TaskList taskList = new TaskList(LISTNAME, "listName");
+        TaskList taskList = new TaskList(1, "First List", "This is my first list");
         taskListDao.save(taskList);
         String listName = taskList.getListName();
 
         //When
-        List<TaskList> readTaskList = taskListDao.findByListName(listName);
+        List<TaskList> readTaskLists = taskListDao.findByListName(listName);
 
         //Then
-        Assert.assertEquals(1, readTaskList.size());
+        Assert.assertEquals(1, readTaskLists.size());
 
         //CleanUp
-        int id = readTaskList.get(0).getId();
-        taskListDao.deleteById(id);
+        int id = readTaskLists.get(0).getId();
+        taskDao.deleteById(id);
     }
+
 }
